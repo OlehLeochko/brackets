adf2684b340455915fa3d2449cc21b0f62d8df5c
Oleh Leochko <olegleochko7@gmail.com>
2020-10-13 00:20:21 +0300

    fix:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str=str.split('');
+//for(let )
+let arr=bracketsConfig.map(el=>{
+  return el.join('')}
+)
+//console.log('str='+str)
+//let arr2=arr1.toString().split("")
+for(let i=0; i<str.length; i++){
+  for(let j=0; j<arr.length; j++){
+    if(str[i]==arr[j]){
+      str=str.filter(item=>item!=str[i]);
+    }
+  }
+}
+if(str.length===0){
+  return true;
+}
+return false;
+//str.length===0?true:false;
 }
