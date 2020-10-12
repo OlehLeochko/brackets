2bafc13a2ebbabffde33c6c3e9628c64cf86354d
Oleh Leochko <olegleochko7@gmail.com>
2020-10-13 00:38:07 +0300

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  str=str.split('');
-//for(let )
+  //str=str.split('');
+
 let arr=bracketsConfig.map(el=>{
   return el.join('')}
 )
 //console.log('str='+str)
 //let arr2=arr1.toString().split("")
-for(let i=0; i<str.length; i++){
+//for(let i=0; i<str.length; i++){
   for(let j=0; j<arr.length; j++){
-    if(str[i]==arr[j]){
-      str=str.filter(item=>item!=str[i]);
+    if(str.includes(arr[j])){
+      str=str.replace(arr[j], '');//str[i]=undefined;
+     // str=str.filter(item=>item!=str[i]);
+     
     }
-  }
+  
 }
 if(str.length===0){
   return true;
